8ddc61548ab9688163fa2b010014c3a8c409d3bd
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-05-01 00:22:35 +0200

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,67 @@
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+	private int port;
+	private String host;
+	private static BufferedReader bufferedReader;
+	private PrintWriter printWriter;
+	private Socket socket;
+
+	public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
+		Client colmin = new Client("127.0.0.1", 8005);
+		colmin.startClient();
+
+		while (true) {
+			colmin.writeMessage("Hello World");
+			readMessage();
+			Thread.sleep(5000);
+		}
+	}
+
+	// Stores the IP address & port nr the client is connecting to
+	public Client(String host, int port) {
+		this.host = host;
+		this.port = port;
+	}
+
+	// Starts the client and sets up streams to and from it
+	private void startClient() throws UnknownHostException, IOException {
+		connectSocket();
+		setupStreams();
+	}
+
+	// Creates a socket to communicate through
+	private void connectSocket() throws UnknownHostException, IOException {
+		socket = new Socket(host, port);
+	}
+
+	// Create IO-Streams
+	private void setupStreams() throws IOException {
+		InputStreamReader input = new InputStreamReader(socket.getInputStream());
+		OutputStreamWriter output = new OutputStreamWriter(socket.getOutputStream());
+
+		bufferedReader = new BufferedReader(input);
+		printWriter = new PrintWriter(output);
+	}
+
+	// new code
+	private static void readMessage() throws IOException {
+		String s = bufferedReader.readLine();
+		System.out.println("Client got message: " + s);
+	}
+
+	// Method to write messages inside the client
+	private void writeMessage(String message) {
+		printWriter.print(message + "\r\n");
+		printWriter.flush();
+	}
+}
---
+++ Server.java
@@ -0,0 +1,72 @@
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+	private int port;
+	private BufferedReader bufferedReader;
+	private PrintWriter printWriter;
+	private ServerSocket socket;
+	private Socket client;
+	private Scanner scanner;
+
+	public static void main(String[] args) throws IOException {
+		Server room5 = new Server(8005);
+		room5.startServer();
+	}
+
+	private Server(int port) {
+		this.port = port;
+	}
+	// Starts the server, sets up streams to and from it and wait for connection
+	private void startServer() throws IOException {
+		socket = openServerSocket(port);
+		client = waitForLogin(socket);
+		System.out.println("connected");
+		setupStreams(client);
+
+		while (true) {
+			String message = readMessage(scanner);
+			writeMessage(message + " arrived at server");
+		}
+	}
+
+	// Creates a socket to communicate through
+	private ServerSocket openServerSocket(int port) throws IOException {
+		socket = new ServerSocket(port);
+		return socket;
+	}
+
+	private Socket waitForLogin(ServerSocket serverSocket) throws IOException {
+		client = serverSocket.accept();
+		return client;
+	}
+
+	// Create IO-Streams
+	private void setupStreams(Socket client) throws IOException {
+		InputStreamReader input = new InputStreamReader(client.getInputStream());
+		OutputStreamWriter output = new OutputStreamWriter(client.getOutputStream());
+
+		scanner = new Scanner(input);
+		printWriter = new PrintWriter(output);
+	}
+
+	// Method to read messages inside the server
+	private String readMessage(Scanner scn) throws IOException {
+		return scn.nextLine();
+	}
+
+	// new code
+	private void writeMessage(String message) {
+		printWriter.print(message + "\r\n");
+		printWriter.flush();
+	}
+}
