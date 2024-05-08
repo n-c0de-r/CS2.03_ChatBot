9bbbb9d59fa322f94f757e2d917fcd451629fca4
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2024-05-08 22:35:03 +0200

    :zap: :memo: Update & documentation

--- Client.java
+++ Client.java
@@ -5,65 +5,117 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
+/**
+ * A very basic Client that connects to a Server.
+ * It takes input messages from the user and sends them to the server.
+ * Not too fancy and might differ from your Lab exercises or implementations.
+ */
 public class Client {
-	private int port;
-	private String host;
-	private static BufferedReader bufferedReader;
-	private PrintWriter printWriter;
-	private Socket socket;
+    private int port;
+    private String host;
+    private static BufferedReader bufferedReader;
+    private PrintWriter printWriter;
+    private Socket socket;
 
-	public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
-		// Client is called “colmin” for Col(in) & (Ner)min :D
-		Client colmin = new Client("127.0.0.1", 8005);
-		colmin.startClient();
+    public static void main(String[] args){
+        Client client = new Client("127.0.0.1", 8005);
+        client.start();
 
-		while (true) {
-			System.out.println("Please enter text:");
-			Scanner s = new Scanner(System.in);
-			String m = s.nextLine();
-			
-			colmin.writeMessage(m);
-			colmin.readMessage();
-			Thread.sleep(5000);
-		}
-	}
+        while (true) {
+            System.out.println("Please enter text:");
+            Scanner s = new Scanner(System.in);
+            String m = s.nextLine();
 
-	// Stores the IP address & port nr the client is connecting to
-	public Client(String host, int port) {
-		this.host = host;
-		this.port = port;
-	}
+            client.writeMessage(m);
+            client.readMessage();
+            try
+            {
+                // wait 5s to display the message, to see if it works
+                Thread.sleep(5000);
+            }
+            catch (InterruptedException ie)
+            {
+                ie.printStackTrace();
+            }
+        }
+    }
 
-	// Starts the client and sets up streams to and from it
-	private void startClient() throws UnknownHostException, IOException {
-		connectSocket();
-		setupStreams();
-	}
+    /**
+     * Stores the IP address & port nr the client is connecting to.
+     * @param host  The String of the host's IP address.
+     * @param port  The port nr to listen to on connection.
+     */ 
+    public Client(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
 
-	// Creates a socket to communicate through
-	private void connectSocket() throws UnknownHostException, IOException {
-		socket = new Socket(host, port);
-	}
+    /**
+     * Starts the client and sets up streams to and from it
+     */ 
+    private void start() {
+        connectSocket();
+        setupStreams();
+    }
 
-	// Create IO-Streams
-	private void setupStreams() throws IOException {
-		InputStreamReader input = new InputStreamReader(socket.getInputStream());
-		OutputStreamWriter output = new OutputStreamWriter(socket.getOutputStream());
+    /**
+     * Creates a socket to communicate through
+     */ 
+    private void connectSocket() {
+        try
+        {
+            socket = new Socket(host, port);
+        }
+        catch (UnknownHostException uhe)
+        {
+            uhe.printStackTrace();
+        }
+        catch (IOException ioe)
+        {
+            ioe.printStackTrace();
+        }
+    }
 
-		bufferedReader = new BufferedReader(input);
-		printWriter = new PrintWriter(output);
-	}
+    // From here on, almost 1:1 copy-paste from Server!
+    /**
+     * Create IO-Streams
+     */ 
+    private void setupStreams() {
+        try (InputStreamReader input = new InputStreamReader(socket.getInputStream());
+        OutputStreamWriter output = new OutputStreamWriter(socket.getOutputStream());)
+        {
+            bufferedReader = new BufferedReader(input);
+            printWriter = new PrintWriter(output);
+        }
+        catch (IOException ioe)
+        {
+            ioe.printStackTrace();
+        }
+    }
 
-	// Read incoming messages and print them out
-	private void readMessage() throws IOException {
-		String s = bufferedReader.readLine();
-		System.out.println("Client got the following: " + s);
-	}
+    /**
+     * Read incoming messages and print them out
+     */ 
+    private void readMessage() {
+        try
+        {
+            String s = bufferedReader.readLine();
+            System.out.println("Client got the following: " + s);
+        }
+        catch (IOException ioe)
+        {
+            ioe.printStackTrace();
+        }
+    }
 
-	// Method to write messages to the server
-	private void writeMessage(String message) {
-		printWriter.print(message + "\r\n");
-		printWriter.flush();
-	}
+    /**
+     * Method to write messages to the server
+     * @param message   The message string to write to the connection.
+     */ 
+    private void writeMessage(String message) {
+        printWriter.print(message + "\r\n");
+        printWriter.flush();
+    }
 }
--- Server.java
+++ Server.java
@@ -6,63 +6,120 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
+/**
+ * A very basic Server that listens to incomming messages and repeats them back.
+ * Not too fancy and might differ from your Lab exercises or implementations.
+ */
 public class Server {
-	private int port;
-	private PrintWriter printWriter;
-	private ServerSocket socket;
-	private Socket client;
-	private Scanner scanner;
+    private int port;
+    private PrintWriter printWriter;
+    private ServerSocket socket;
+    private Socket client;
+    private Scanner scanner;
 
-	public static void main(String[] args) throws IOException {
-		// We were in breakout room nr 5, so sour server and port reflect that :D
-		Server room5 = new Server(8005);
-		room5.startServer();
-	}
+    public static void main(String[] args) throws IOException {
+        // We were in breakout room nr 5, so our server and port reflect that :D
+        Server room5 = new Server(8005);
+        room5.startServer();
+    }
+    
+    /**
+     * Creates a Server listening to the given port.
+     * @param port  The port number to listen to.
+     */
+    private Server(int port) {
+        this.port = port;
+    }
 
-	private Server(int port) {
-		this.port = port;
-	}
-	// Starts the server, sets up streams to and from it and wait for connection
-	private void startServer() throws IOException {
-		socket = openServerSocket(port);
-		client = waitForLogin(socket);
-		System.out.println("connected");
-		setupStreams(client);
+    /**
+     * Starts the server, sets up streams to and from it and wait for connection
+     */ 
+    private void startServer() {
+        socket = openServerSocket(port);
+        if(socket != null)
+            client = waitForLogin(socket);
+            
+        if(client != null)
+            setupStreams(client);
 
-		while (true) {
-			String message = readMessage(scanner);
-			writeMessage(message + "'" + message + "' arrived at server");
-		}
-	}
+        while (true) {
+            String message = readMessage(scanner);
+            writeMessage(message + "'" + message + "' arrived at server");
+        }
+    }
 
-	// Creates a socket to communicate through
-	private ServerSocket openServerSocket(int port) throws IOException {
-		socket = new ServerSocket(port);
-		return socket;
-	}
+    /**
+     * Creates a socket to communicate through
+     * @param port  The port number to connect to.
+     * @returns A ServerSocket on successful connection.
+     */ 
+    private ServerSocket openServerSocket(int port) {
+        try
+        {
+            socket = new ServerSocket(port);
+            
+            if(socket != null)
+                System.out.println("connected");
+            
+                return socket;
+        }
+        catch (IOException ioe)
+        {
+            ioe.printStackTrace();
+        }
+        return null;
+    }
+    
+    /**
+     * Tries to connect to a ServerSocket and returns a client Socket.
+     * @param serverSocket  The ServerSocket to connect the client to.
+     * @returns A Socket connected to the Server, representing the Client.
+     */
+    private Socket waitForLogin(ServerSocket serverSocket) {
+        try
+        {
+            client = serverSocket.accept();
+            return client;
+        }
+        catch (IOException ioe)
+        {
+            ioe.printStackTrace();
+        }
+        return null;
+    }
 
-	private Socket waitForLogin(ServerSocket serverSocket) throws IOException {
-		client = serverSocket.accept();
-		return client;
-	}
+    /**
+     * Create IO-Streams
+     * @param client    The client socket to read and write to.
+     */ 
+    private void setupStreams(Socket client) {
+        try (InputStreamReader input = new InputStreamReader(client.getInputStream());
+        OutputStreamWriter output = new OutputStreamWriter(client.getOutputStream()))
+        {
+            scanner = new Scanner(input);
+            printWriter = new PrintWriter(output);
+        } 
+        catch (IOException ioe)
+        {
+            ioe.printStackTrace();
+        }
+    }
 
-	// Create IO-Streams
-	private void setupStreams(Socket client) throws IOException {
-		InputStreamReader input = new InputStreamReader(client.getInputStream());
-		OutputStreamWriter output = new OutputStreamWriter(client.getOutputStream());
+    /**
+     * Method to read messages from the client
+     * @param scn   Scanner class to read input from.
+     * @returns The String of the input.
+     */
+    private String readMessage(Scanner scn) {
+        return scn.nextLine();
+    }
 
-		scanner = new Scanner(input);
-		printWriter = new PrintWriter(output);
-	}
-
-	// Method to read messages from the client
-	private String readMessage(Scanner scn) throws IOException {
-		return scn.nextLine();
-	}
-
-	// Method to write messages back to the client
-	private void writeMessage(String message) {
-		printWriter.print(message + "\r\n");
-		printWriter.flush();
-	}
+    /**
+     * Method to write messages back to the client
+     * @param message   The received message to print to the screen.
+     */ 
+    private void writeMessage(String message) {
+        printWriter.print(message + "\r\n");
+        printWriter.flush();
+    }
 }
